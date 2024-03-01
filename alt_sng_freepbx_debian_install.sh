c11c4ab8183e0035ce5ff974c75d4cc66e9ae7f2
chrsmj <52975048+chrsmj@users.noreply.github.com>
2024-03-01 14:06:33 -0700

    Added alternate bash installer script
    Bootstrap Ansible on the TARGET, using new alternate
    installer, making it more of a drop-in replacement
    for the existing installer; however, retain the power
    to still run with Ansible on your CONTROL machine,
    should you choose to flex your DevOps muscles, and,
    say, install FreePBX on a dozen hosts at once.

---
+++ alt_sng_freepbx_debian_install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2024 Penguin PBX Solutions <chris at penguin p b x dot com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Bootstrap of Ansible to install Sangoma FreePBX and Asterisk on Debian 12.
+#
+# You can run this directly on your TARGET machine.
+
+# Check if we are bash shell
+if [ "x${EUID}" = "x" ]; then
+  echo "This script must be run in bash"
+  exit 1
+fi
+
+# Check if we are root privileged
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
+# Initialize logging
+LOG_FILE='/var/log/pbx/freepbx17-install-alt.log'
+mkdir -p '/var/log/pbx/'
+
+# The rest will tee to the log
+{
+
+echo -n "START: " && date
+
+# Include executed commands in the log
+set -x
+
+cd /root
+
+# Bootstrap minimal Ansible install, and get git
+apt-get -qq install ansible git
+
+# Assume we only downloaded this shell script, so get the repo
+git clone https://github.com/chrsmj/sng_freepbx_debian_install.git
+
+# Get to the right branch in the repo
+cd sng_freepbx_debian_install/
+git checkout ansible-ize
+cd ansible-role/sng_freepbx_debian/
+
+# Run the Ansible playbook locally (normally over SSH from CONTROL to TARGET)
+ansible-playbook -i localhost, --connection=local playbook.yml
+
+# Clean up the Ansible bits (mostly python libs)
+apt-get -qq remove ansible
+apt-get -qq autoremove
+
+# Finish the command logging
+set +x
+
+echo -n "STOP: " && date
+
+} 2>&1 | tee -a $LOG_FILE
