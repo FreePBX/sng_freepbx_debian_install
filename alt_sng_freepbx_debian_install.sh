6a519a4d6e9f6c33c89a70264fa6b242369a3e8d
chrsmj <52975048+chrsmj@users.noreply.github.com>
2024-03-26 11:13:40 -0600

    now parsing command line args to be an even closer match to current installer

--- alt_sng_freepbx_debian_install.sh
+++ alt_sng_freepbx_debian_install.sh
@@ -7,9 +7,19 @@
 # Bootstrap of Ansible to install Sangoma FreePBX and Asterisk on Debian 12.
 #
 # You can run this directly on your TARGET machine.
+# It supports the same command line arguments as the main installer.
+#
+set -e
+SCRIPTVER="1.1-alt"
+ASTVERSION=21
+AACVERSION="2.0.1-1"
+PHPVERSION="8.2"
+LOG_FOLDER="/var/log/pbx"
+LOG_FILE="${LOG_FOLDER}/freepbx17-install-alt-$(date '+%Y.%m.%d-%H.%M.%S').log"
+DISTRIBUTION="$(lsb_release -is)"
 
 # Check if we are bash shell
-if [ "x${EUID}" = "x" ]; then
+if [ "${EUID}" = "" ]; then
   echo "This script must be run in bash"
   exit 1
 fi
@@ -21,8 +31,44 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Initialize logging
-LOG_FILE='/var/log/pbx/freepbx17-install-alt.log'
-mkdir -p '/var/log/pbx/'
+mkdir -p "${LOG_FOLDER}"
+echo "" > $LOG_FILE
+
+# Get parameters
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		--testing)
+			testrepo=true
+			sfpd_install_testing=true # for ansible
+			shift # past argument
+			;;
+		--nofreepbx)
+			nofpbx=true
+			sfpd_install_freepbx=false # for ansible
+			shift # past argument
+			;;
+		--noasterisk)
+			noast=true
+			sfpd_install_asterisk=false # for ansible
+			shift # past argument
+			;;
+		--noioncube)
+			noioncube=true
+			sfpd_install_ioncube=false # for ansible
+			shift # past argument
+			;;
+		-*|--*)
+			echo "Unknown option $1"
+			exit 1
+			;;
+		*)
+			POSITIONAL_ARGS+=("$1") # save positional arg
+			shift # past argument
+			;;
+	esac
+done
 
 # The rest will tee to the log
 {
@@ -32,7 +78,7 @@ echo -n "START: " && date
 # Include executed commands in the log
 set -x
 
-cd /root
+cd /root || exit
 
 # Bootstrap minimal Ansible install, plus git, python3, and lsb-release
 apt-get -qq install ansible git python3 lsb-release
@@ -41,12 +87,23 @@ apt-get -qq install ansible git python3 lsb-release
 git clone https://github.com/chrsmj/sng_freepbx_debian_install.git
 
 # Get to the right branch in the repo
-cd sng_freepbx_debian_install/
+cd sng_freepbx_debian_install/ || exit
 git checkout ansible-ize
-cd ansible-role/sng_freepbx_debian/
+cd ansible-role/sng_freepbx_debian/ || exit
 
 # Run the Ansible playbook locally (normally over SSH from CONTROL to TARGET)
-ansible-playbook -i localhost, --connection=local playbook.yml
+ansible-playbook \
+  --inventory localhost, \
+  --become-method=su \
+  --connection=local \
+  --extra-vars "sfpd_install_asterisk=${sfpd_install_asterisk}" \
+  --extra-vars "sfpd_install_freepbx=${sfpd_install_freepbx}" \
+  --extra-vars "sfpd_install_ioncube=${sfpd_install_ioncube}" \
+  --extra-vars "sfpd_install_testing=${sfpd_install_testing}" \
+  --extra-vars "sfpd_asterisk_version=${ASTVERSION}" \
+  --extra-vars "sfpd_aac_version=${AACVERSION}" \
+  --extra-vars "pngnx_php_version=${PHPVERSION}" \
+  playbook.yml
 
 # Clean up the Ansible bits (mostly python libs)
 apt-get -qq remove ansible
