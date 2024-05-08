efdd786de3c2ec2b6a6773dbb0bf93e8bcd0b3f6
kapil gupta <kgupta@sangoma.com>
2024-05-07 22:43:13 -0400

    updating repos first

--- sng_freepbx_debian_install.sh
+++ sng_freepbx_debian_install.sh
@@ -473,6 +473,7 @@ setCurrentStep "Making sure installation is same"
 # Fixing broken install
 apt -y --fix-broken install >> $log 2>&1
 apt autoremove -y >> "$log" 2>&1
+apt update >> $log 2>&1
 
 # Adding iptables and postfix  inputs so "iptables-persistent" and postfix will not ask for the input
 setCurrentStep "Setting up default configuration"
