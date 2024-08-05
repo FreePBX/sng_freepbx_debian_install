3db42ccea57e20f5048cd3c1c0595a94c899311c
kapil gupta <kgupta@sangoma.com>
2024-08-05 08:17:31 -0400

    bumping up the version

--- sng_freepbx_debian_install.sh
+++ sng_freepbx_debian_install.sh
@@ -32,7 +32,7 @@ fi
 if ! dpkg -l | grep -q wget >> /dev/null 2>&1; then
     apt install -y wget >> /dev/null 2>&1
 fi
-SCRIPTVER="1.5"
+SCRIPTVER="1.6"
 ASTVERSION=21
 AACVERSION="2.0.1-1"
 PHPVERSION="8.2"
