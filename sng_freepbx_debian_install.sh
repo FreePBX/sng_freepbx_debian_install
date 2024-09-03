a08a3eac0dda46eb8236d30a0e34404acdd93874
kapil gupta <kgupta@sangoma.com>
2024-09-03 07:35:18 -0400

    bumping up the version

--- sng_freepbx_debian_install.sh
+++ sng_freepbx_debian_install.sh
@@ -22,7 +22,7 @@
 #                                               FreePBX 17                          #
 #####################################################################################
 set -e
-SCRIPTVER="1.9"
+SCRIPTVER="1.10"
 ASTVERSION=21
 PHPVERSION="8.2"
 LOG_FOLDER="/var/log/pbx"
